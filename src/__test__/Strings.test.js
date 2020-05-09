fde519eb5c8f440a08833b1c2ecfeb8a170d32de
José García Máñez <josegarciamanez@gmail.com>
2020-05-09 16:03:37 +0200

    add numbers test

--- src/__test__/Strings.test.js
+++ src/__test__/Strings.test.js
@@ -1,9 +1,13 @@
-import { saludo, getId } from '../utils/strings';
+import { saludo, getId, despedida } from '../utils/strings';
 
 describe('Pruebas con strings', () => {
   test('Probando funcion de saludo', () => {
     const saludar = saludo('Juan');
     expect(saludar).toMatch('Hola, me llamo Juan');
+  });
+  test('Probando funcion de despedida', () => {
+    const despedirse = despedida();
+    expect(despedirse).not.toMatch('Adios');
   });
   test('Probando funcion getId', () => {
     const id = getId();
--- src/utils/strings.js
+++ src/utils/strings.js
@@ -1,6 +1,9 @@
 export const saludo = (name) => {
   return 'Hola, me llamo ' + name;
 };
+export const despedida = () => {
+  return 'Bye';
+};
 export const getId = (name) => {
   return '22-436';
 };
