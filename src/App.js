d0ff75ab3dc8eae937ce12102b2d04b8dc65708e
José García Máñez <josegarciamanez@gmail.com>
2020-05-10 10:51:42 +0200

    selecting components in enzyme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from 'react';
+
+export const Title = () => <h1>Introduccion a Unit Testing</h1>;
 
 const App = () => {
   return (
-    <div>
-      <h1>
-        Introduccion a Unit Testing
-      </h1>
-    </div>
-  )
-}
+    <section id='main'>
+      <Title />
+      <div className='container'>
+        <span num={3} active={false}>
+          Primero
+        </span>
+        <span num='3' active='false'>
+          Segundo
+        </span>
+      </div>
+      <input type='text' />
+      <input type='checkbox' />
+    </section>
+  );
+};
 
-export default App
+export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { configure, shallow, mount, render } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import App from './App';
+import App, { Title } from './App';
 
 configure({
   adapter: new Adapter(),
@@ -11,11 +11,13 @@ describe('Probando componente <App />', () => {
   test('Probando renderizado de componente', () => {
     const wrapper = shallow(<App />);
 
-    expect(wrapper.find('h1').html()).toBe(
-      '<h1>Introduccion a Unit Testing</h1>'
-    );
-    expect(wrapper.find('h1')).toHaveLength(1);
+    // console.log(wrapper.find('h1'))
+    // console.log(wrapper.find('div ~ p').length)
+    // console.log(wrapper.find('[type="checkbox"]').html())
+    // console.log(wrapper.find(Title).html())
 
+    // expect(wrapper.find('h1').html()).toBe('<h1>Introduccion a Unit Testing</h1>')
+    // expect(wrapper.find('h1')).toHaveLength(1)
     // expect(wrapper.html()).toBe('<div><h1>Introduccion a Unit Testing</h1></div>')
   });
 });
